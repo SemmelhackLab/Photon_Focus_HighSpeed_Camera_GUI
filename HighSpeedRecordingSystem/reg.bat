274c95a36c9289c08e9d8329ea2f93bf2e5ff25e
Biswadeep Khan <biswadeepkhan@gmail.com>
2023-10-20 19:38:30 +0800

    Add files via upload

---
+++ HighSpeedRecordingSystem/reg.bat
@@ -0,0 +1,7 @@
+regsvr32 C:\HighSpeedRecordingSystem\spcomm.dll /s
+regsvr32 C:\HighSpeedRecordingSystem\CMoreFileMap.dll /s
+regsvr32 C:\HighSpeedRecordingSystem\CCasheMap.dll /s
+regsvr32 C:\HighSpeedRecordingSystem\Silicon.dll /s
+regsvr32 C:\HighSpeedRecordingSystem\ComCtrl.dll /s
+regsvr32 C:\HighSpeedRecordingSystem\CUdpCtrl.dll /s
+regsvr32 C:\HighSpeedRecordingSystem\CPhotonDll.dll /s
---
+++ HighSpeedRecordingSystem/ureg.bat
@@ -0,0 +1,7 @@
+regsvr32 C:\HighSpeedRecordingSystem\spcomm.dll /u /s
+regsvr32 C:\HighSpeedRecordingSystem\CMoreFileMap.dll /u /s
+regsvr32 C:\HighSpeedRecordingSystem\CCasheMap.dll /u /s
+regsvr32 C:\HighSpeedRecordingSystem\Silicon.dll /u /s
+regsvr32 C:\HighSpeedRecordingSystem\ComCtrl.dll /u /s
+regsvr32 C:\HighSpeedRecordingSystem\CUdpCtrl.dll /u /s
+regsvr32 C:\HighSpeedRecordingSystem\CPhotonDll /u /s
